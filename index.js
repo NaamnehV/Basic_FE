8bca8ed24695ac948c0b3d2d23cca1b753cc8a3f
NaamnehV <135263424+NaamnehV@users.noreply.github.com>
2023-08-28 16:29:50 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,121 @@
+// со скидкой 10%
+
+// let commodity_price = parseFloat(prompt ("enter the price of the item" ));
+// if (!isNaN(commodity_price)) {
+//     let discount = commodity_price * 0.1;
+//     let discount_price = commodity_price - discount;
+//     console.log ("price with 10% discount : " + discount_price);
+// } 
+// else{
+//     console.log("enter a valid number");
+// }
+
+// с наценкой 10%
+
+// let commodity_price = parseFloat(prompt ("enter the price of the item" ));
+// if (!isNaN(commodity_price)) {
+//     let discount = commodity_price * 0.1;
+//     let discount_price = commodity_price + discount;
+//     console.log ("price with 10% discount : " + discount_price);
+// } 
+// else{
+//     console.log("enter a valid number");
+// }
+
+
+// со скидкой 25%
+
+// let commodity_price = parseFloat(prompt ("enter the price of the item" ));
+// if (!isNaN(commodity_price)) {
+//     let discount = commodity_price * 0.25;
+//     let discount_price = commodity_price - discount;
+//     console.log ("price with 10% discount : " + discount_price);
+// } 
+// else{
+//     console.log("enter a valid number");
+// }
+
+// с наценкой 25%
+
+// let commodity_price = parseFloat(prompt ("enter the price of the item" ));
+// if (!isNaN(commodity_price)) {
+//     let discount = commodity_price * 0.25;
+//     let discount_price = commodity_price + discount;
+//     console.log ("price with 10% discount : " + discount_price);
+// } 
+// else{
+//     console.log("enter a valid number");
+// }
+
+// со скидкой 1%
+
+// let commodity_price = parseFloat(prompt ("enter the price of the item" ));
+// if (!isNaN(commodity_price)) {
+//     let discount = commodity_price * 0.01;
+//     let discount_price = commodity_price - discount;
+//     console.log ("price with 10% discount : " + discount_price);
+// } 
+// else{
+//     console.log("enter a valid number");
+// }
+
+// с наценкой 1%
+
+// let commodity_price = parseFloat(prompt ("enter the price of the item" ));
+// if (!isNaN(commodity_price)) {
+//     let discount = commodity_price * 0.01;
+//     let discount_price = commodity_price + discount;
+//     console.log ("price with 10% discount : " + discount_price);
+// } 
+// else{
+//     console.log("enter a valid number");
+// }
+
+// 1.
+
+// let price = parseFloat(prompt(" enter the price of the item "));
+// if(price >=100) {
+//     let discount = price * 0.05;
+//     let discount_price = price-discount;
+//     console.log("discount price : " + discount_price);
+// }
+// else {
+//     console.log("price without discount " + price);
+// }
+
+
+// 2.
+
+// let user_number = parseFloat(prompt("Enter the number : "));
+// if (user_number > 0 ) {
+//     console.log(" Number dreaten than 0 ");
+// } else if (user_number < 0) {
+//     console.log(" number less than 0 ");
+// } else  {
+//     console.log(" Number is 0 ");
+// }
+
+// 3.
+// let user_number = parseFloat(prompt("Enter the number : "));
+// let last_digit = user_number % 10;
+// console.log(" The last digit of the number : " + last_digit);
+
+// 4. 
+// sum_numbers
+// calculate_average
+// is_even 
+// is_prime
+// format_date
+// capitalize_first_letters
+// reverse_array
+// generate_random_nummers
+// content_upper_case
+// lower_case_string
+// is_valid_email
+// find_max_value
+// find_min_value
+// calculate_geometry_area
+// convert_temperature
+// calculate_factorial
+// calculate_distance
+// format_currency
