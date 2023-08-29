58aa463d840e0767ee5b1332127b2ed980d8d190
NaamnehV <135263424+NaamnehV@users.noreply.github.com>
2023-08-29 18:48:38 +0300

    Add files via upload

---
+++ index24,08.js
@@ -0,0 +1,17 @@
+
+// 2.
+// function largest_number(array) {
+//     if (array.length === 0) {
+//       return undefined; 
+//     }
+    
+//     return Math.max(...array);
+//   }
+// 3.
+// function even_oss_numbers(array) {
+//     if (array.length % 2 === 0) {
+//       return "even numbers of elements";
+//     } else {
+//       return " odd numbers of elements";
+//     }
+//   }
